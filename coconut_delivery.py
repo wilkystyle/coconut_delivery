45adc3027f45109e24b538471b5c714599214d76
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2015-01-03 21:58:33 -0500

    Adding __init__ methods, filling out build_flight function

--- coconut_delivery.py
+++ coconut_delivery.py
@@ -3,16 +3,6 @@ import sys
 DATA_FILE = "sample_paths.txt"
 
 
-def build_flight(data_file):
-    """
-    Build and return a flight object from a given input file.
-    """
-    # TODO
-    raise NotImplementedError(
-        "You have not yet implemented the build_flight method!"
-    )
-
-
 class Flight:
     """
     The delivery flight.
@@ -23,6 +13,11 @@ class Flight:
     jetstreams = []
     distance = 0
     base_cost = 0
+
+    def __init__(self, jetstreams, distance, base_cost):
+        self.jetstreams = jetstreams
+        self.distance = distance
+        self.base_cost = base_cost
 
 
 class Jetstream:
@@ -35,11 +30,52 @@ class Jetstream:
     start = 0
     end = 0
     cost = 0
+    score = 0
+
+    def __init__(self, start, end, cost):
+        self.start = start
+        self.end = end
+        self.cost = cost
+
+
+def build_flight(data_file):
+    """
+    Build and return a flight object from a given input file.
+    """
+
+    # Initialize the jetstreams list with placeholder for the start position.
+    jetstreams = [Jetstream(0, 0, 0)]
+    base_cost = int(data_file.readline())
+    distance = 0  # Also known as the end of the route.
+
+    for line in data_file.readlines():
+        start, end, cost = [int(x) for x in line.split()]
+
+        if end > distance:
+            distance = end
+
+        jetstreams.append(
+            Jetstream(
+                start,
+                end,
+                cost,
+            )
+        )
+
+    # Add a placeholder for the end position.
+    jetstreams.append(distance, distance, 0)
+
+    return Flight(
+        jetstreams,
+        distance,
+        base_cost,
+    )
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         DATA_FILE = sys.argv[1]
 
+    flight = None
     with open(DATA_FILE) as f:
-        build_flight(f)
+        flight = build_flight(f)
