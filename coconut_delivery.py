05250fc015becd5038907b05f504e928882ecf57
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2015-01-04 00:22:36 -0500

    Adding methods to Jetstream class, and updating vars

--- coconut_delivery.py
+++ coconut_delivery.py
@@ -26,23 +26,68 @@ class Jetstream:
     end = 0
     cost = 0
     score = 0
+    next_stream = None
+    flight = None
 
-    def __init__(self, start, end, cost):
+    def __init__(self, start, end, cost, flight):
         self.start = start
         self.end = end
         self.cost = cost
+        self.flight = flight
+
+    def ensure_base_score(self):
+        """
+        Set a base score is set for this jetstream, if we haven't already.
+        """
+        dist = self.flight.distance
+        base_cost = self.flight.base_cost
+
+        if self.score == 0:
+            # No base score yet. Calculate one.
+            cost_to_end = ((dist - self.end) * base_cost)
+            self.score = self.cost + cost_to_end
+
+    def update_score_maybe(self, other_stream):
+        """
+        Update the score and next_stream of this Jetstream if the other stream
+        results in a better score for the this one.
+        """
+        # First, let's make sure this node has a base score.
+        self.ensure_base_score()
+        base_cost = self.flight.base_cost
+
+        # See if using the other stream next would result in a better score.
+        gap_cost = ((other_stream.start - self.end) * base_cost)
+        additional_cost = other_stream.score + gap_cost
+        new_score = self.cost + additional_cost
+
+        if new_score < self.score:
+            self.score = new_score
+            self.next_stream = other_stream
+
+    def enumerate_path(self):
+        """
+        List the jetstreams from the current stream to the end of the flight.
+        """
+        current_stream = self.next_stream
+        while current_stream:
+            yield (current_stream.start, current_stream.end)
+            current_stream = current_stream.next_stream
 
 
 def build_flight(data_file):
     """
-    Build and return a flight object from a given input file.
+    Build and return a flight object from a given input file. The jetstreams
+    for the flight will be sorted by their ending position, in descending
+    order.
     """
-
-    # Initialize the jetstreams list with placeholder for the start position.
-    jetstreams = [Jetstream(0, 0, 0)]
-    base_cost = int(data_file.readline())
+    the_flight = Flight()
+    the_flight.base_cost = int(data_file.readline())  # First line is base cost
     distance = 0  # Also known as the end of the route.
+    # Jetstreams list starts with a placeholder node for the starting position.
+    jetstreams = [Jetstream(0, 0, 0, the_flight)]
 
+    # Read in the remaining lines, creating a Jetstream object for each one.
     for line in data_file.readlines():
         start, end, cost = [int(x) for x in line.split()]
 
